d3dfd683db2aa815f141c27db5a0515108ea46ad
mdrokz <mohammadmunshi@gmail.com>
2021-02-06 15:04:25 +0530

    inlined Util Objects

--- screeps/Utils.hx
+++ screeps/Utils.hx
@@ -7,18 +7,18 @@ import js.lib.Object;
 @:forward
 abstract JsMap<T, K>(Map<T, K>) {
 	@:arrayAccess
-	function get(key:T):K {
+	public inline function get(key:T):K {
 		return js.Syntax.code("{0}[{1}]", this, key);
 	}
 
-	public function new() {
+	public inline function new() {
 		this = new Map<T, K>();
 	}
 }
 
 abstract JsObject<T>(Object) {
 	@:arrayAccess
-	function get(key:String):T {
+	public inline function get(key:String):T {
 		return js.Syntax.code("{0}[{1}]", this, key);
 		// return [for(x in Object.entries(this)) x.key == key ? x : continue][0].value;
 		// return Object.entries(this).filter(k -> k.key == key)[0].value;
